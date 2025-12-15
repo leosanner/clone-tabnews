df731b65a01ee2a706726d59b09194413edb0e0e
Leonardo Sanner <leosanner12@gmail.com>
2025-12-15 13:57:27 -0300

    add:

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const queryString = "SELECT 1 + 1";
+  const result = await database.query(queryString);
+
+  console.log(result);
+
   const resp = {
     chave: "são acima da média",
     headers: request.headers,
