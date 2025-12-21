880cc40078ebe18e14cc3d7e3bc7b5486337ab08
Leonardo Sanner <leosanner12@gmail.com>
2025-12-21 17:31:05 -0300

    query parametrizadas com node-postgres e informações de conexões do banco com testes integrados

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const status = response.status;
+  const responseBody = await response.json();
 
+  console.log(responseBody);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody);
+
+  expect(responseBody.dependencies.database.openned_connections).toBe(1);
   expect(status).toBe(200);
 });
