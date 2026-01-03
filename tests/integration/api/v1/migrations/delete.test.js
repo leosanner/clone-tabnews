439264880a09d9a0027eff21c0f07de698222a08
Leonardo Sanner <leosanner12@gmail.com>
2026-01-03 12:17:28 -0300

    ajuste no teste

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -1,13 +1,7 @@
 test("DELETE for http://localhost/api/v1/migrations should return 405", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "DELETE",
   });
 
-  expect(response1.status).toBe(405);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "GET",
-  });
-
-  expect(response2.status).toBe(200);
+  expect(response.status).toBe(405);
 });
