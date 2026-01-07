4cd0cb6eb6670c96bba35f8494709420f2e38086
Leonardo Sanner <leosanner12@gmail.com>
2026-01-07 09:58:41 -0300

    atualização no script ´dev´ inserindo função para verificar se conexão com o banco de dados está funcionando

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,17 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+    console.log("\n\n✅ Postgres aceitando conexões.");
+  }
+}
+
+process.stdout.write("\n\n⏳ Aguardando postgres aceitar conexões");
+checkPostgres();
