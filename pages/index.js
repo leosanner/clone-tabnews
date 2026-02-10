f24a43cc67c4bbfa56259c92fb63f2032c035ea3
Leonardo Sanner <leosanner12@gmail.com>
2026-02-10 13:05:06 -0300

    fix linting

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import { Image } from "next/image";
 function Home() {
   return (
     <>
       <h1>Vitor orbigado por ser meu amigão!</h1>
-      <Image src="/img/image.png" alt="" />
-      <Image src="/img/image1.png" alt="" />
     </>
   );
 }
