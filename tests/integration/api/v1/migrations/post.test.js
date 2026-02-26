bc232aa8ff9c35c0b367f8fe589891e09d22fc46
Leonardo Sanner <leosanner12@gmail.com>
2026-02-26 17:24:38 -0300

    style: updated content

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -23,7 +23,6 @@ test("POST to /api/v1/migrations should return 200", async () => {
   const firstMigrationObject = responseBody[0];
 
   expect(responseBody.length).toBeGreaterThan(0);
-  expect(firstMigrationObject.path.startsWith("infra")).toBe(true);
 
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
