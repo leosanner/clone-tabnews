3842e93092f99d8eef1240ad7df00539d6585d07
Leonardo Sanner <leosanner12@gmail.com>
2026-03-15 13:34:28 -0300

    feat: add handler for status api

--- infra/errors.js
+++ infra/errors.js
@@ -12,7 +12,26 @@ export class InternalServerError extends Error {
     return {
       name: this.name,
       message: this.message,
-      actions: this.action,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint.", {});
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP enviado é válido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
       status_code: this.statusCode,
     };
   }
